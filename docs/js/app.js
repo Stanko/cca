6b58f8b3c5e1ac1b3232d9013865f33c4bfb1a23
Stanko <stanko.tadic@gmail.com>
2024-10-28 09:33:34 +0100

    Reach out button, close button

--- docs/js/app.js
+++ docs/js/app.js
@@ -19,12 +19,22 @@ updateScrollbarWidth();
 
 const $modal = document.querySelector('.modal');
 const $modalClose = document.querySelector('.modal-close');
+const $modalBackdropClose = document.querySelector('.modal-backdrop-close');
 const $contactTrigger = document.querySelector('.elastic-button--contact');
+const $reachOutTrigger = document.querySelector('.reach-out');
 
 $contactTrigger.addEventListener('click', () => {
   $modal.showModal();
 });
 
+$reachOutTrigger.addEventListener('click', () => {
+  $modal.showModal();
+});
+
 $modalClose.addEventListener('click', () => {
+  $modal.close();
+});
+
+$modalBackdropClose.addEventListener('click', () => {
   $modal.close();
 });
