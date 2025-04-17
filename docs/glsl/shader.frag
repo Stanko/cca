380d1956e2d4dc8cfa02d3f59f3c84f14002313b
Aidan Wyber <pandakaas@gmail.com>
2025-04-17 19:56:13 +0200

    removed mouse interaction, added logo animation

--- docs/glsl/shader.frag
+++ docs/glsl/shader.frag
@@ -11,7 +11,6 @@ varying vec2 vTexCoord;
 
 uniform float time;
 uniform vec2 resolution;
-uniform vec2 mouse;
 
 #define TIME (time * 0.1)
 #define SCALE 1.
@@ -27,15 +26,14 @@ float sdCCA(in vec2 pos) {
 
 void main() {
   vec2 pos = px2ShaderSpace(vTexCoord * resolution);
-  vec2 mousePos = px2ShaderSpace(mouse);
 
   mat2 M = mat2(0.2, -0.8, 1.1, -0.6) * TIME * TAU;
   M = mat2(cos(M[0][0]), cos(M[0][1]), cos(M[1][0]), cos(M[1][1]));// * 0.5 + 0.5;
   M += mat2(1.3, 0.9, -0.48, 1.4);
   M /= M[0][0] * M[1][1] - M[1][0] * M[0][1];
 
-  vec2 m = M * (pos - mousePos);
-  vec3 col = vec3(pos, 0.) + vec3(0., mousePos) + vec3(0., abs(m));
+  vec2 m = M * pos;
+  vec3 col = vec3(pos, 0.) + vec3(0., abs(m));
   col += 1. - step(0.1, length(m) - 0.5);
 
   gl_FragColor = vec4(col, 1.0);
--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -4,6 +4,9 @@ let time;
 let theShader;
 let isPlaying = true;
 
+let ccaPos;
+let ccaRadius = 25.4;
+let isDraggingCcaPos = false;
 
 function preload() {
   theShader = loadShader('./glsl/shader.vert', './glsl/shader.frag');
@@ -13,6 +16,8 @@ function preload() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.id('p5Canvas');
+
+  ccaPos = createVector(0, -height/2 + 110);
 }
 
 
@@ -23,12 +28,91 @@ function draw() {
 
   theShader.setUniform("time", time);
   theShader.setUniform("resolution", [width, height]);
-  theShader.setUniform("mouse", [mouseX, height - 1 - mouseY]); // flip y for glsl
+  theShader.setUniform("ccaPxPos", [ccaPos.x, height - 1 - ccaPos.y]); // flip y for glsl
+  theShader.setUniform("ccaRadius", ccaRadius);
 
   shader(theShader);
 
   rectMode(CENTER);
   rect(0, 0, width, height);
+
+  push();
+  resetShader();
+  translate(ccaPos.x, ccaPos.y);
+  blendMode(DIFFERENCE);
+  drawCCA();
+  pop();
+
+  if (mouseIsPressed && isDraggingCcaPos) {
+    setCursor('grabbing');
+  }
+  else {
+    setCursor(isCursorOverCCA() ? 'grab' : 'default');
+  }
+
+  if (isDraggingCcaPos) {
+    ccaPos.x = lerp(ccaPos.x, mouseX - width/2, 0.2);
+    ccaPos.y = lerp(ccaPos.y, mouseY - height/2, 0.2);
+  }
+}
+
+function setCursor(cursorType) {
+  canvas.elt.style.cursor = cursorType;
+}
+
+
+function isCursorOverCCA() {
+  return mouseX - width/2 < ccaPos.x + ccaRadius * 3 && mouseX - width/2 > ccaPos.x - ccaRadius * 3 &&
+  mouseY - height/2 < ccaPos.y + ccaRadius && mouseY - height/2 > ccaPos.y - ccaRadius;
+}
+
+
+function drawCCA() {
+  noFill();
+  strokeWeight(ccaRadius * 0.4);
+  const q = pow(
+    map(
+      -cos(
+        constrain(
+          fract(time * 0.1) / 0.1, 0, 1
+        ) * PI
+      ), -1, 1, 0, 1
+    ), 1
+  );
+  const q2 = pow(
+    map(
+      -cos(
+        constrain(
+          fract(time * 0.1) / 0.1, 0, 1
+        ) * TAU
+      ), -1, 1, 0, 1
+    ), 1
+  );
+  const offs = q2 * ccaRadius * 2;
+  const rot = q * TAU;
+
+  // CC
+  stroke(255);
+  for (let i = -1; i <= 0; i++) {
+    push();
+    {
+      translate(i * ccaRadius * 2 + i * offs, 0);
+      rotate(rot);
+      arc(0, 0, ccaRadius * 2, ccaRadius * 2, PI/4, PI*7/4);
+    }
+    pop();
+  }
+
+  // A
+  stroke('#ff6868');
+  push();
+  {
+    translate(ccaRadius * 2 + offs, 0);
+    rotate(rot);
+    circle(0, 0, ccaRadius * 2);
+    line(ccaRadius, 0, ccaRadius, ccaRadius);
+  }
+  pop();
 }
 
 
@@ -45,6 +129,15 @@ function keyPressed() {
     fullscreen(!fs);
     break;
   }
+}
+
+function mousePressed() {
+  if (isCursorOverCCA()) 
+    isDraggingCcaPos = true;
+
+}
+function mouseReleased() {
+  isDraggingCcaPos = false;
 }
 
 
