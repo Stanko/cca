fee89af5f2d12a863990828274506fe8a3a24cb9
Stanko <stanko.tadic@gmail.com>
2025-08-20 12:50:52 +0200

    Invaders page updated with submissions

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,12 +4,12 @@ const updateScrollbarWidth = () => {
   const scrollbarWidth =
     window.innerWidth - document.documentElement.clientWidth;
   document.documentElement.style.setProperty(
-    '--scrollbar-width',
-    `${scrollbarWidth}px`
+    "--scrollbar-width",
+    `${scrollbarWidth}px`,
   );
 };
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   updateScrollbarWidth();
 });
 
@@ -17,24 +17,56 @@ updateScrollbarWidth();
 
 // ----- Modal ----- //
 
-const $modal = document.querySelector('.modal');
-const $modalClose = document.querySelector('.modal-close');
-const $modalBackdropClose = document.querySelector('.modal-backdrop-close');
-const $contactTrigger = document.querySelector('.elastic-button--contact');
-const $reachOutTrigger = document.querySelector('.reach-out');
+const $modal = document.querySelector(".modal");
+const $modalClose = document.querySelector(".modal-close");
+const $modalBackdropClose = document.querySelector(".modal-backdrop-close");
+const $contactTrigger = document.querySelector(".elastic-button--contact");
+const $reachOutTrigger = document.querySelector(".reach-out");
 
-$contactTrigger.addEventListener('click', () => {
+$contactTrigger.addEventListener("click", () => {
   $modal.showModal();
 });
 
-$reachOutTrigger.addEventListener('click', () => {
-  $modal.showModal();
-});
+if ($reachOutTrigger) {
+  $reachOutTrigger.addEventListener("click", () => {
+    $modal.showModal();
+  });
+}
 
-$modalClose.addEventListener('click', () => {
+$modalClose.addEventListener("click", () => {
   $modal.close();
 });
 
-$modalBackdropClose.addEventListener('click', () => {
+$modalBackdropClose.addEventListener("click", () => {
   $modal.close();
 });
+
+// ----- Invaders ----- //
+
+const $submissionsPause = document.querySelector(".submissions__pause");
+const $submissionsVideos = [...document.querySelectorAll(".submissions video")];
+
+if ($submissionsPause) {
+  // Check for auto play
+  const isPaused = $submissionsVideos.some(($video) => $video.paused);
+
+  if (isPaused) {
+    $submissionsPause.textContent = "Play videos";
+  } else {
+    $submissionsPause.textContent = "Pause videos";
+  }
+
+  $submissionsPause.addEventListener("click", () => {
+    const isPaused = $submissionsVideos.some(($video) => $video.paused);
+
+    $submissionsVideos.forEach(($video) => {
+      if (isPaused) {
+        $video.play();
+        $submissionsPause.textContent = "Pause videos";
+      } else {
+        $video.pause();
+        $submissionsPause.textContent = "Play videos";
+      }
+    });
+  });
+}
